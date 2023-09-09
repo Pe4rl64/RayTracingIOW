5f2f8fd10feb186ac6c6608fb09ae893446dfb0d
Adnaan Juma <105602441+Pe4rl64@users.noreply.github.com>
2023-09-09 12:56:10 +0200

    Added some more operators to Vec3 class
    Added scalar product and division, == (and !=) comparison operator, and dot, cross, unit vector methods.
    These last ones are yet to be fully implemented.

--- RayTracingIOW/src/Vec3.cpp
+++ RayTracingIOW/src/Vec3.cpp
@@ -23,9 +23,24 @@ Vec3 Vec3::operator*(const Vec3& other) const
 	return Vec3(x * other.x, y * other.y, z * other.z);
 }
 
+Vec3 Vec3::operator*(float scalar) const
+{
+	return Vec3(x * scalar, y * scalar, z * scalar);
+}
+
+Vec3 operator*(float scalar, const Vec3& vector)
+{
+	return vector * scalar;
+}
+
 Vec3 Vec3::operator/(const Vec3& other) const
 {
 	return Vec3(x / other.x, y / other.y, z / other.z);
+}
+
+Vec3 Vec3::operator/(float scalar) const
+{
+	return (*this) * (1 / scalar);
 }
 
 void Vec3::operator+=(const Vec3& other)
@@ -49,6 +64,13 @@ void Vec3::operator*=(const Vec3& other)
 	z *= other.z;
 }
 
+void Vec3::operator*=(float scalar)
+{
+	x *= scalar;
+	y *= scalar;
+	z *= scalar;
+}
+
 void Vec3::operator/=(const Vec3& other) 
 {
 	x /= other.x;
@@ -56,9 +78,31 @@ void Vec3::operator/=(const Vec3& other)
 	z /= other.z;
 }
 
+void Vec3::operator/=(float scalar)
+{
+	x /= scalar;
+	y /= scalar;
+	z /= scalar;
+}
+
 float Vec3::length() const
 {
 	return std::sqrt(x * x + y * y + z * z);
+}
+
+float Vec3::cross(const Vec3& other) const
+{
+	return 0.0f;
+}
+
+float Vec3::dot(const Vec3& other) const
+{
+	return 0.0f;
+}
+
+Vec3 Vec3::unitVector() const
+{
+	return (*this) / length();
 }
 
 std::ostream& operator<<(std::ostream& stream, const Vec3& vector)
--- RayTracingIOW/src/Vec3.h
+++ RayTracingIOW/src/Vec3.h
@@ -15,16 +15,28 @@ public:
 	Vec3 operator-() const;
 	Vec3 operator-(const Vec3& other) const;
 	Vec3 operator*(const Vec3& other) const;
+	Vec3 operator*(float scalar) const;
+	friend Vec3 operator*(float scalar, const Vec3& vector);
 	Vec3 operator/(const Vec3& other) const;
+	Vec3 operator/(float scalar) const;
 	
 	void operator+=(const Vec3& other);
 	void operator-=(const Vec3& other);
 	void operator*=(const Vec3& other);
+	void operator*=(float scalar);
 	void operator/=(const Vec3& other);
+	void operator/=(float scalar);
+
+	bool operator==(const Vec3& other) const = default;
 
 	float length() const;
+	float cross(const Vec3& other) const;
+	float dot(const Vec3& other) const;
+	Vec3 unitVector() const;
 
 	float x, y, z;
 };
+
+
 
 std::ostream& operator<<(std::ostream& stream, const Vec3& vector);
