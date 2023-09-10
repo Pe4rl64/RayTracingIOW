0cadb7b8af6003de868bf3cd6763942b1590e823
Adnaan Juma <adnaanjuma06@gmail.com>
2023-09-10 20:30:59 +0200

    Implemented Camera, Viewport and Ray concepts
    Rays are now generated for each pixel of the viewport, starting from a camera position to the pixel location on the viewport.
    The color of the pixel is determined by a function that takes in a ray and decides the color accordingly.

--- RayTracingIOW/src/Main.cpp
+++ RayTracingIOW/src/Main.cpp
@@ -4,29 +4,70 @@
 #include "Vec3.h"
 #include "Color.h"
 #include "Point3.h"
+#include "Ray.h"
+
+Color rayColor(const Ray& ray)
+{
+	return Color(0, 0, 0);
+}
+
+Color randomRayColor(const Ray& ray)
+{
+	return Color(static_cast<float>((rand() % 256)) / 255, static_cast<float>((rand() % 256)) / 255, static_cast<float>((rand() % 256)) / 255);
+}
 
 int main()
 {
 	// Image
 	std::ofstream image("res/image.ppm");
 
-	constexpr uint32_t IMAGE_WIDTH = 256;
-	constexpr uint32_t IMAGE_HEIGHT = 256;
+	float aspectRatio = 16.0f / 9.0f; // ideal aspect ratio
+	uint32_t imageWidth = 400;
+
+	// Calculating image height, which must be at least 1
+	uint32_t imageHeight = static_cast<int>(imageWidth / aspectRatio);
+	
+	if (imageHeight < 1)
+		imageHeight = 1;
+
+	// Camera
+	float focalLenght = 1.0f;
+	float viewportHeight = 2.0f;
+	float viewportWidth = viewportHeight * (static_cast<float>(imageWidth) / imageHeight); // Viewport width can be less than 1 since real valued. Uses real aspect ratio
+	Point3 cameraCenter(0, 0, 0);
+
+	// Vector across the horizontal edge of the viewport (starting from the top left corner)
+	Vec3 viewportHorizontal(viewportWidth, 0, 0);
+	// Vector across the vertical edge of the viewport (starting from the top left corner)
+	Vec3 viewportVertical(0, -viewportHeight, 0);
+
+	// Horizontal delta vector from pixel to pixel
+	Vec3 pixelDeltaHorizontal = viewportHorizontal / imageWidth;
+	// Vertical delta vector from pixel to pixel
+	Vec3 pixelDeltaVertical = viewportVertical / imageHeight;
+
+	// Upper left corner of the viewport
+	Point3 viewportUpperLeft = cameraCenter 
+		- Vec3(0, 0, focalLenght)
+		- ((viewportHorizontal + viewportVertical) / 2);
+	// Upper left pixel 
+	Point3 pixelUpperLeft = viewportUpperLeft
+		+ ((pixelDeltaHorizontal + pixelDeltaVertical) / 2);
 
 	// Render
-	image << "P3\n" << IMAGE_WIDTH << ' ' << IMAGE_HEIGHT << "\n255\n";
+	image << "P3\n" << imageWidth << ' ' << imageHeight << "\n255\n";
 
-	for (int i = 0; i < IMAGE_HEIGHT; i++)
+	for (int i = 0; i < imageHeight; i++)
 	{
-		std::cout << "Scanlines remaining: " << IMAGE_WIDTH - i << "\n";
+		std::cout << "Scanlines remaining: " << imageHeight - i << "\n";
 
-		for (int j = 0; j < IMAGE_WIDTH; j++)
+		for (int j = 0; j < imageWidth; j++)
 		{
-			Color pixelColor = Color(float(i) / (IMAGE_HEIGHT - 1),
-				float(j) / (IMAGE_WIDTH - 1),
-				0.5f);
+			Point3 pixelCurrent = pixelUpperLeft + (j * pixelDeltaHorizontal) + (i * pixelDeltaVertical);
+			Vec3 rayDirection = cameraCenter + pixelCurrent;
+			Ray ray(cameraCenter, rayDirection);
 			
-			image << pixelColor << '\n';
+			image << randomRayColor(ray) << '\n';
 		}
 	}
 
